ae2c3af171dd7cf5de8784a5f6e3d76d0c1c1f0f
董岳 <minidy20001001@outlook.com>
2021-07-28 06:20:57 +0800

    Add project files.

---
+++ DrivingLightsSim/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using DrivingLightsSim.Services;
+using DrivingLightsSim.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DrivingLightsSim
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ DrivingLightsSim/Models/LightCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrivingLightsSim.Models
+{
+    public class LightCommand
+    {
+        public string Content { get; set; }
+
+        public AnswerType Answer { get; set; }
+
+        public enum AnswerType
+        {
+            LOW, HIGH, LOW_HIGH, FLASHING,
+        }
+    }
+}
